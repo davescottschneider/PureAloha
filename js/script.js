a807a6d4c8fdafd2a7f08b00235058cdd6a3a545
Dave Schneider <umdave@gmail.com>
2014-05-24 14:37:43 -0400

    created main style sheet and added css grid for responsive features, created empty javascript file

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// You are viewing the jQuery script for Pure Aloha! developed
+// by Dave Scott Schneider. To hire Dave call him at (734)755-8132. 
+
+$(document).ready(function(){
+	// Cache the Window object
+	$window = $(window);
+});
