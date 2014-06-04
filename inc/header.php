4b51c17d69e75498e0dc23df65088a8440f29f10
Dave Schneider <umdave@gmail.com>
2014-06-04 12:20:16 -0400

    installed bootstrap

--- inc/header.php
+++ inc/header.php
@@ -1,12 +1,14 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+	<meta charset="UTF-8">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
 	<title><?php echo $pageTitle; ?></title>
-	<meta charset="UTF-8">
 	<link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
 	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
 	<link rel="stylesheet" href="css/normalize.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
 	<script src="js/script.js"></script>
+	<script src="js/bootstrap.js"></script>
 </head>
