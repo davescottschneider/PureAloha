95b1bda21513bf711c725a215fb7c704033d9fa6
davescottschneider <umdave@gmail.com>
2014-07-11 12:28:54 -0400

    Added mobile responsive styles to homepage

--- index.php
+++ index.php
@@ -33,67 +33,69 @@ $pageDesc="The meta description for the Pure Aloha home page.";
 	</div>
 	<section class="srcollFade" id="content">
 		 <section class="ocean">
-		 	<div class="container clearfix tour" id="whale">
-		 		<h2>Kauai Whale Watching Tours</h2>
-		 		<div class="col-md-7 left">
-		 			<p>Hawaii is home to over 24 different species of cetaceans. Most people come on our tours expecting to see humpback whales but we commonly see various other creatures including spinner dolphins, spotted dolphin and bottlenose dolphins as well as sea turtles and other ocean friends.</p>
-		 			<h3>A Top Whale Watching Destination</h3>
-		 			<p>Highlight your Kauai vacation with a majestic whale watching tour. Hawaii consistently place among the top rated destinations for whale watching. Veteran whale watchers agree that our whale watching tours rank among the finest in terms of our boat, crew, value and experience. Make sure you bring your camera on this unforgettable journey.</p>
-		 		</div>
-		 		<div class="col-md-4">
-		 			<img src="img/snorkel.png" title="" alt=""/>
-		 			<a href="#" class="btn btn-lg btn-success col-md-5">Learn More</a>
-		 			<a href="#" class="btn btn-lg btn-primary col-md-5">Book Now</a>
-		 		</div>
-		 	</div>
-		 	<div class="container clearfix tour" id="fishing">
-		 		<h2>Kauai Charter Fishing Tours</h2>
-		 		<div class="col-md-7 block">
-		 			<p>Whether it is the thrill of fighting a big fish or catching healthy, tasty dinner is what you're looking for, you've come to the right place. Kauai has some of the best deep sea fishing around as our waters are home to a wide variety of big game species. Your catch may include Amberjack, Barracuda, Cobia, Mahi-Mahi, Grouper, Kingfish, Marlin, Sailfish, Shark, Snapper, Swordfish, Tuna and Wahoo. </p>
-		 			<h3>Cast Your Pole and Wait For The Tug</h3>
-		 			<p>When you feel a tug on your line the action can quickly become hectic; the last thing you need is an inexperienced crew. Pure Aloha has an experienced, professional and exciting crew that will do whatever it takes to help you land your trophy catch. Our guys were born and raised fishing on the island and will provide an experience that is unparalleled to any charter around.</p>
-		 		</div>
-		 		<div class="col-md-4">
-		 			<img src="img/snorkel.png" title="" alt=""/>
-		 			<img src="" title="" alt=""/>
-		 		</div>
-		 	</div>
-		 	<div class="container clearfix tour" id="napali">
-		 		<h2>Na Pali Coast Tours</h2>
-		 		<div class="col-md-7 left">
-		 			<p>The Na Pali Coast is located on the northwest shore of Kauai and boasts 15 miles of rugged coastline. Inaccessible by road, these picturesque views are only available by sea or to the experienced hiker. The best way to enjoy this one-of-a-kind shoreline is on a cruise with Pure Aloha.</p>
-		 			<h3>Unspoiled Coastline</h3>
-		 			<p>Enjoy the view of the cliffs as they drop straight down thousands of feet to the coastline. The Na Pali Coast is filled with waterfalls and secret coves we can’t wait to explore with you. Join us on a journey that is unparalleled to any other experience.</p>
-		 		</div>
-		 		<div class="col-md-4">
-		 			<img src="img/snorkel.png" title="" alt=""/>
-		 			<img src="" title="" alt=""/>
-		 		</div>
-		 	</div>
-		 	<div class="container clearfix tour" id="snorkeling">
-		 		<h2>Snorkeling Tours in Kauai</h2>
-		 		<div class="col-md-7 right">
-		 			<p>Some of the most beautiful places in Kauai are underwater. Kauai is home to thousands of different types of sea creatures, many of which are visible on our snorkeling tours. Spend your afternoon exploring reefs and observing beautiful foliage as you swim with sea turtles, sea horses, dolphins and kaleidoscope of colorful fish including the state fish of Hawaii, Humuhumunukunukuapua`a (don’t worry, we’ll sound that one out for you).</p>
-		 			<h3>A Day of Sun and Sea Life</h3>
-		 			<p>We take our guests on an excursion to secluded coves only accessible by boat and provide a rare opportunity to see a hidden side of Kauai. Whether you are a veteran snorkeler or this will be your first time out, we have the perfect experience for you. Or crew was raised in these waters and knows the very best places to explore the wonders of the sea.</p>
-		 		</div>
-		 		<div class="col-md-4">
-		 			<img src="img/snorkel.png" title="" alt=""/>
-		 			<img src="" title="" alt=""/>
-		 		</div>
-		 	</div>
-		 	<div class="container clearfix tour" id="sunset">
-		 		<h2>Sunset Cruises</h2>
-		 		<div class="col-md-7 left">
-		 			<p>Leave your worries behind as we sail off into Kauai’s famous sunset and watch the sun paint the sky with colors that change from brilliant blues to deep yellows, oranges, pinks and reds. Reflect on another perfect day in paradise and watch the sun slowly disappear beyond the horizon as you relax and enjoy the beauty of Mother Nature.</p>
-		 			<h3>Experience Paradise</h3>
-		 			<p>When you join us for a cruise into the tropical waters of Kauai with a warm ocean breeze and a magnificent sunset as a backdrop, your vacation will be complete. Experience a sunset sail with Pure Aloha and you will fully understand what island paradise is all about. Our sunset cruises are extremely popular, so book early.</p>
-		 		</div>
-		 		<div class="col-md-4">
-		 			<img src="img/snorkel.png" title="" alt=""/>
-		 			<img src="" title="" alt=""/>
-		 		</div>
-		 	</div>
+		 	<div class="content">
+			 	<div class="container clearfix tour" id="whale">
+			 		<h2>Kauai Whale Watching Tours</h2>
+			 		<div class="col-md-7 left">
+			 			<p>Hawaii is home to over 24 different species of cetaceans. Most people come on our tours expecting to see humpback whales but we commonly see various other creatures including spinner dolphins, spotted dolphin and bottlenose dolphins as well as sea turtles and other ocean friends.</p>
+			 			<h3>A Top Whale Watching Destination</h3>
+			 			<p>Highlight your Kauai vacation with a majestic whale watching tour. Hawaii consistently place among the top rated destinations for whale watching. Veteran whale watchers agree that our whale watching tours rank among the finest in terms of our boat, crew, value and experience. Make sure you bring your camera on this unforgettable journey.</p>
+			 		</div>
+			 		<div class="col-md-5">
+			 			<img src="img/snorkel.png" title="" alt=""/>
+			 			<a href="#" class="btn btn-lg btn-success col-md-5">Learn More</a>
+			 			<a href="#" class="btn btn-lg btn-primary col-md-5">Book Now</a>
+			 		</div>
+			 	</div>
+			 	<div class="container clearfix tour" id="fishing">
+			 		<h2>Kauai Charter Fishing Tours</h2>
+			 		<div class="col-md-7 block">
+			 			<p>Whether it is the thrill of fighting a big fish or catching healthy, tasty dinner is what you're looking for, you've come to the right place. Kauai has some of the best deep sea fishing around as our waters are home to a wide variety of big game species. Your catch may include Amberjack, Barracuda, Cobia, Mahi-Mahi, Grouper, Kingfish, Marlin, Sailfish, Shark, Snapper, Swordfish, Tuna and Wahoo. </p>
+			 			<h3>Cast Your Pole and Wait For The Tug</h3>
+			 			<p>When you feel a tug on your line the action can quickly become hectic; the last thing you need is an inexperienced crew. Pure Aloha has an experienced, professional and exciting crew that will do whatever it takes to help you land your trophy catch. Our guys were born and raised fishing on the island and will provide an experience that is unparalleled to any charter around.</p>
+			 		</div>
+			 		<div class="col-md-4">
+			 			<img src="img/snorkel.png" title="" alt=""/>
+			 			<img src="" title="" alt=""/>
+			 		</div>
+			 	</div>
+			 	<div class="container clearfix tour" id="napali">
+			 		<h2>Na Pali Coast Tours</h2>
+			 		<div class="col-md-7 left">
+			 			<p>The Na Pali Coast is located on the northwest shore of Kauai and boasts 15 miles of rugged coastline. Inaccessible by road, these picturesque views are only available by sea or to the experienced hiker. The best way to enjoy this one-of-a-kind shoreline is on a cruise with Pure Aloha.</p>
+			 			<h3>Unspoiled Coastline</h3>
+			 			<p>Enjoy the view of the cliffs as they drop straight down thousands of feet to the coastline. The Na Pali Coast is filled with waterfalls and secret coves we can’t wait to explore with you. Join us on a journey that is unparalleled to any other experience.</p>
+			 		</div>
+			 		<div class="col-md-4">
+			 			<img src="img/snorkel.png" title="" alt=""/>
+			 			<img src="" title="" alt=""/>
+			 		</div>
+			 	</div>
+			 	<div class="container clearfix tour" id="snorkeling">
+			 		<h2>Snorkeling Tours in Kauai</h2>
+			 		<div class="col-md-7 left">
+			 			<p>Some of the most beautiful places in Kauai are underwater. Kauai is home to thousands of different types of sea creatures, many of which are visible on our snorkeling tours. Spend your afternoon exploring reefs and observing beautiful foliage as you swim with sea turtles, sea horses, dolphins and kaleidoscope of colorful fish including the state fish of Hawaii, Humuhumunukunukuapua`a (don’t worry, we’ll sound that one out for you).</p>
+			 			<h3>A Day of Sun and Sea Life</h3>
+			 			<p>We take our guests on an excursion to secluded coves only accessible by boat and provide a rare opportunity to see a hidden side of Kauai. Whether you are a veteran snorkeler or this will be your first time out, we have the perfect experience for you. Or crew was raised in these waters and knows the very best places to explore the wonders of the sea.</p>
+			 		</div>
+			 		<div class="col-md-4">
+			 			<img src="img/snorkel.png" title="" alt=""/>
+			 			<img src="" title="" alt=""/>
+			 		</div>
+			 	</div>
+			 	<div class="container clearfix tour" id="sunset">
+			 		<h2>Sunset Cruises</h2>
+			 		<div class="col-md-7 left">
+			 			<p>Leave your worries behind as we sail off into Kauai’s famous sunset and watch the sun paint the sky with colors that change from brilliant blues to deep yellows, oranges, pinks and reds. Reflect on another perfect day in paradise and watch the sun slowly disappear beyond the horizon as you relax and enjoy the beauty of Mother Nature.</p>
+			 			<h3>Experience Paradise</h3>
+			 			<p>When you join us for a cruise into the tropical waters of Kauai with a warm ocean breeze and a magnificent sunset as a backdrop, your vacation will be complete. Experience a sunset sail with Pure Aloha and you will fully understand what island paradise is all about. Our sunset cruises are extremely popular, so book early.</p>
+			 		</div>
+			 		<div class="col-md-4">
+			 			<img src="img/snorkel.png" title="" alt=""/>
+			 			<img src="" title="" alt=""/>
+			 		</div>
+			 	</div>
+			 </div>
 	 	</section>
 	</section>
 	<div id="bubble-container">
