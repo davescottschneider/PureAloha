c3c01312931cb9a97a44f5ca6a112c42353af426
davescottschneider <umdave@gmail.com>
2014-07-15 10:26:29 -0400

    Added new styles for mobile, deployed new relic

--- about.php
+++ about.php
@@ -9,12 +9,16 @@ $section="about";
 	 </div>
 	 <div class="container clearfix aboutcontent">
 	 	<div class="content">
-		 	<h2>Aloha!</h2>
-			<p>That means <em>Welcome</em> in Hawaiian. Sed sed diam vitae magna luctus aliquam. Nunc imperdiet leo et neque mollis accumsan. Fusce tempus velit at ante semper, in rhoncus neque volutpat. Aenean in urna sed massa lobortis porttitor. Proin eu blandit libero. Etiam id ligula et tellus tincidunt porttitor. Fusce mattis imperdiet pharetra. Maecenas lorem mauris, pulvinar nec est sit amet, tristique convallis quam. Maecenas lectus eros, iaculis ac bibendum nec, fringilla in justo.</p>
-		 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla metus dolor, faucibus eleifend suscipit vitae, auctor a urna. Maecenas ac quam quis mi sagittis hendrerit vel eget urna. Suspendisse ac ante magna. Cras varius posuere erat, fringilla venenatis ante bibendum eget. Ut at arcu a diam elementum fermentum. Integer a enim quis dui faucibus consectetur ac vitae odio.</p>
-			<h2>Meet The Crew</h2>
-			<p>Sed sed diam vitae magna luctus aliquam. Nunc imperdiet leo et neque mollis accumsan. Fusce tempus velit at ante semper, in rhoncus neque volutpat. Aenean in urna sed massa lobortis porttitor. Proin eu blandit libero. Etiam id ligula et tellus tincidunt porttitor. Fusce mattis imperdiet pharetra. Maecenas lorem mauris, pulvinar nec est sit amet, tristique convallis quam. Maecenas lectus eros, iaculis ac bibendum nec, fringilla in justo.</p>
-		 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla metus dolor, faucibus eleifend suscipit vitae, auctor a urna. Maecenas ac quam quis mi sagittis hendrerit vel eget urna. Suspendisse ac ante magna. Cras varius posuere erat, fringilla venenatis ante bibendum eget. Ut at arcu a diam elementum fermentum. Integer a enim quis dui faucibus consectetur ac vitae odio.</p>
+	 		<div class="col-md-8">
+			 	<h2>Aloha!</h2>
+				<p>That means <em>Welcome</em> in Hawaiian. Sed sed diam vitae magna luctus aliquam. Nunc imperdiet leo et neque mollis accumsan. Fusce tempus velit at ante semper, in rhoncus neque volutpat. Aenean in urna sed massa lobortis porttitor. Proin eu blandit libero. Etiam id ligula et tellus tincidunt porttitor. Fusce mattis imperdiet pharetra. Maecenas lorem mauris, pulvinar nec est sit amet, tristique convallis quam. Maecenas lectus eros, iaculis ac bibendum nec, fringilla in justo.</p>
+			 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla metus dolor, faucibus eleifend suscipit vitae, auctor a urna. Maecenas ac quam quis mi sagittis hendrerit vel eget urna. Suspendisse ac ante magna. Cras varius posuere erat, fringilla venenatis ante bibendum eget. Ut at arcu a diam elementum fermentum. Integer a enim quis dui faucibus consectetur ac vitae odio.</p>
+			</div>
+			<div class="col-md-10">
+				<h2>Meet The Crew</h2>
+				<p>Sed sed diam vitae magna luctus aliquam. Nunc imperdiet leo et neque mollis accumsan. Fusce tempus velit at ante semper, in rhoncus neque volutpat. Aenean in urna sed massa lobortis porttitor. Proin eu blandit libero. Etiam id ligula et tellus tincidunt porttitor. Fusce mattis imperdiet pharetra. Maecenas lorem mauris, pulvinar nec est sit amet, tristique convallis quam. Maecenas lectus eros, iaculis ac bibendum nec, fringilla in justo.</p>
+		 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla metus dolor, faucibus eleifend suscipit vitae, auctor a urna. Maecenas ac quam quis mi sagittis hendrerit vel eget urna. Suspendisse ac ante magna. Cras varius posuere erat, fringilla venenatis ante bibendum eget. Ut at arcu a diam elementum fermentum. Integer a enim quis dui faucibus consectetur ac vitae odio.</p>
+	 		</div>
 	 	</div>
 	 </div>
 <?php include('inc/footer.php'); ?>
