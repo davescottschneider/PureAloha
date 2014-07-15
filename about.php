1e2127f02dbaef51301704036b0ed2d5e1069cef
davescottschneider <umdave@gmail.com>
2014-07-15 11:39:52 -0400

    Added pictures as placeholders for about page

--- about.php
+++ about.php
@@ -9,15 +9,18 @@ $section="about";
 	 </div>
 	 <div class="container clearfix aboutcontent">
 	 	<div class="content">
-	 		<div class="col-md-8">
-			 	<h2>Aloha!</h2>
+	 		<h2 class="col-md-12">Aloha!</h2>
+	 		<div class="col-md-7">
 				<p>That means <em>Welcome</em> in Hawaiian. Sed sed diam vitae magna luctus aliquam. Nunc imperdiet leo et neque mollis accumsan. Fusce tempus velit at ante semper, in rhoncus neque volutpat. Aenean in urna sed massa lobortis porttitor. Proin eu blandit libero. Etiam id ligula et tellus tincidunt porttitor. Fusce mattis imperdiet pharetra. Maecenas lorem mauris, pulvinar nec est sit amet, tristique convallis quam. Maecenas lectus eros, iaculis ac bibendum nec, fringilla in justo.</p>
 			 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla metus dolor, faucibus eleifend suscipit vitae, auctor a urna. Maecenas ac quam quis mi sagittis hendrerit vel eget urna. Suspendisse ac ante magna. Cras varius posuere erat, fringilla venenatis ante bibendum eget. Ut at arcu a diam elementum fermentum. Integer a enim quis dui faucibus consectetur ac vitae odio.</p>
 			</div>
-			<div class="col-md-10">
-				<h2>Meet The Crew</h2>
+			<img class="col-md-5 vspacer" src="img/boat2.jpg" alt="The Pure Aloha, our flagship" title="Aloha! Welcome to Pure Aloha Ocean Adventures" />
+			<h2 class="col-md-12">Meet The Crew</h2>
+			<div class="col-md-12">
+				<img src="img/leigirl.jpg" class="col-md-4" alt="The captain of the Pure Aloha, Joe Schmo." title="Kauai Tour Captain, Joe Schmo"/>
+				<h3>The Captain</h3>
 				<p>Sed sed diam vitae magna luctus aliquam. Nunc imperdiet leo et neque mollis accumsan. Fusce tempus velit at ante semper, in rhoncus neque volutpat. Aenean in urna sed massa lobortis porttitor. Proin eu blandit libero. Etiam id ligula et tellus tincidunt porttitor. Fusce mattis imperdiet pharetra. Maecenas lorem mauris, pulvinar nec est sit amet, tristique convallis quam. Maecenas lectus eros, iaculis ac bibendum nec, fringilla in justo.</p>
-		 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla metus dolor, faucibus eleifend suscipit vitae, auctor a urna. Maecenas ac quam quis mi sagittis hendrerit vel eget urna. Suspendisse ac ante magna. Cras varius posuere erat, fringilla venenatis ante bibendum eget. Ut at arcu a diam elementum fermentum. Integer a enim quis dui faucibus consectetur ac vitae odio.</p>
+		 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla metus dolor, faucibus eleifend suscipit vitae, auctor a urna. Maecenas ac quam quis mi sagittis hendrerit vel eget urna. Suspendisse ac ante magna. Cras varius posuere erat, fringilla venenatis ante bibendum eget.</p>
 	 		</div>
 	 	</div>
 	 </div>
