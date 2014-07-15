637226eb97bcb5abe1bed4e6fc3eede7b5b2eaeb
davescottschneider <umdave@gmail.com>
2014-07-15 12:19:48 -0400

    Added findus page content, altered regpage styles for responsiveness

--- findus.php
+++ findus.php
@@ -1,5 +1,22 @@
 <?php
 $pageClass="findus regpage";
-$pageTitle="Pure Aloha! A Hawaiian Paradise | Find Us in Kauai | Kauai Fishing trip, Kauai Na Pali Coast Tours, Kauai Whale Watching";
+$pageTitle="Pure Aloha! A Hawaiian Paradise | About | Na Pali Coast Tours, Kauai Whale Watching Tours, Kauai Charter Fishing";
+$pageDesc="A SEO description of the about page";
 $section="findus";
  include('inc/header.php'); ?>
+	 <div class="marquee">
+	 	<h1>Find Us On Kauai</h1>
+	 </div>
+	 <div class="container clearfix aboutcontent">
+	 	<div class="content">
+	 		<div class="col-md-6">
+	 			<h2>Directions</h2>
+				<p>Insert Directions Here</p>
+			</div>
+			<div class="col-md-6">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d236670.53277373518!2d-159.540196!3d22.05047245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c0704bb259eb047%3A0xf6321af5a12d378a!2sKauai%2C+Hawaii!5e0!3m2!1sen!2sus!4v1405441013409" width="600" height="450" frameborder="0" style="border:0"></iframe>
+			</div>
+			
+	 	</div>
+	 </div>
+<?php include('inc/footer.php'); ?>
