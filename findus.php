f972b69904c6f900afacd85bbbed84dfaf107f4f
davescottschneider <umdave@gmail.com>
2014-07-15 12:29:52 -0400

    Added responsive style for google maps

--- findus.php
+++ findus.php
@@ -14,9 +14,12 @@ $section="findus";
 				<p>Insert Directions Here</p>
 			</div>
 			<div class="col-md-6">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d236670.53277373518!2d-159.540196!3d22.05047245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c0704bb259eb047%3A0xf6321af5a12d378a!2sKauai%2C+Hawaii!5e0!3m2!1sen!2sus!4v1405441013409" width="600" height="450" frameborder="0" style="border:0"></iframe>
+				<div class="google-maps">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d236670.53277373518!2d-159.540196!3d22.05047245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c0704bb259eb047%3A0xf6321af5a12d378a!2sKauai%2C+Hawaii!5e0!3m2!1sen!2sus!4v1405441013409" width="600px" height="450px" frameborder="0" style="border:0"></iframe>
+				</div>
 			</div>
 			
 	 	</div>
 	 </div>
 <?php include('inc/footer.php'); ?>
+width="600" height="450"
